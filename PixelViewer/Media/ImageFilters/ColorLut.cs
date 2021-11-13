95d8f1e58682549285ac234f5129b8a0d49977e5
Hamster <hamster620@gmail.com>
2021-11-13 09:40:33 +0800

    Add ColorLutImageFilter.

---
+++ PixelViewer/Media/ImageFilters/ColorLut.cs
@@ -0,0 +1,65 @@
+﻿using CarinaStudio;
+using System;
+using System.Collections.Generic;
+
+namespace Carina.PixelViewer.Media.ImageFilters
+{
+    /// <summary>
+    /// Methods for generating color Loookup Table (LUT).
+    /// </summary>
+    static class ColorLut
+    {
+        /// <summary>
+        /// Build LUT with identity transformation.
+        /// </summary>
+        /// <param name="targetFormat">Target bitmap format.</param>
+        /// <returns>LUT.</returns>
+        public static IList<double> BuildIdentity(BitmapFormat targetFormat)
+        {
+            return (targetFormat switch
+            {
+                BitmapFormat.Bgra32 => new double[256],
+                BitmapFormat.Bgra64 => new double[65536],
+                _ => throw new ArgumentException($"Unsupported format: {targetFormat}"),
+            }).Also(it => ResetToIdentity(it));
+        }
+
+
+        /// <summary>
+        /// Apply gamma transformation.
+        /// </summary>
+        /// <param name="lut">LUT.</param>
+        /// <param name="gamma">Gamma.</param>
+        public static void GammaTransform(IList<double> lut, double gamma) =>
+            GammaTransform(lut, 0, lut.Count, gamma);
+
+
+        // Apply gamma transformation on specific range of LUT.
+        static void GammaTransform(IList<double> lut, int start, int end, double gamma)
+        {
+            // check range
+            var count = (double)(end - start - 1);
+            if (count < -0.1)
+                return;
+
+            // apply
+            for (var n = start; n < end; ++n)
+            {
+                var input = (n - start) / count;
+                if (Math.Abs(lut[n]) > 0.0001)
+                    lut[n] *= Math.Pow(input, gamma);
+            }
+        }
+
+
+        /// <summary>
+        /// Reset LUT to identity transformation.
+        /// </summary>
+        /// <param name="lut">LUT.</param>
+        public static void ResetToIdentity(IList<double> lut)
+        {
+            for (var n = lut.Count - 1; n >= 0; --n)
+                lut[n] = n;
+        }
+    }
+}
