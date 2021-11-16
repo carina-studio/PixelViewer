b1911cab00e4964d00281ac266aa56463fff2560
Hamster <hamster620@gmail.com>
2021-11-16 13:38:06 +0800

    Use readable string in label of image format category.

---
+++ PixelViewer/Data/Converters/EnumConverters.cs
@@ -0,0 +1,17 @@
+﻿using Carina.PixelViewer.Media;
+using CarinaStudio.AppSuite.Converters;
+using System;
+
+namespace Carina.PixelViewer.Data.Converters
+{
+    /// <summary>
+    /// Common converters for enumerations.
+    /// </summary>
+    static class EnumConverters
+    {
+        /// <summary>
+        /// Convert from <see cref="ImageFormatCategory"/>.
+        /// </summary>
+        public static readonly EnumConverter ImageFormatCategory = new EnumConverter(App.CurrentOrNull, typeof(ImageFormatCategory));
+    }
+}
