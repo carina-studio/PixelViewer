84ce419bdf5ec0da168766018cbf362786befa4b
Hamster <hamster620@gmail.com>
2022-02-17 17:00:32 +0800

    Set releasing type back to Preview.

--- PixelViewer/App.axaml.cs
+++ PixelViewer/App.axaml.cs
@@ -348,7 +348,7 @@ namespace Carina.PixelViewer
 
 
 		// Releasing type.
-		public override ApplicationReleasingType ReleasingType => ApplicationReleasingType.ReleaseCandidate;
+		public override ApplicationReleasingType ReleasingType => ApplicationReleasingType.Preview;
 
 
 		// Version of settings.
