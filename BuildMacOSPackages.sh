093da5753921761d3008cdaea0f9647cac4684cb
Hamster <hamster620@gmail.com>
2022-08-07 08:17:11 +0800

    Update build script for macOS.

--- BuildMacOSPackages.sh
+++ BuildMacOSPackages.sh
@@ -4,21 +4,24 @@ PUB_PLATFORM_LIST=("osx-x64" "osx-arm64")
 CONFIG="Release"
 CERT_NAME="" # Name of certification to sign the application
 
-# Reset output directory
-rm -r ./Packages
-mkdir ./Packages
-if [ "$?" != "0" ]; then
-    exit
-fi
+echo "********** Start building $APP_NAME **********"
 
 # Get application version
-VERSION=$(cat ./$APP_NAME/$APP_NAME.csproj | grep "<Version>" | egrep -o "[0-9]+(.[0-9]+)+")
-if [ "$VERSION" == "" ]; then
+VERSION=$(dotnet run --project PackagingTool get-current-version $APP_NAME/$APP_NAME.csproj)
+if [ "$?" != "0" ]; then
     echo "Unable to get version of $APP_NAME"
     exit
 fi
-echo " " 
-echo "******************** Build $APP_NAME $VERSION ********************"
+echo "Version: $VERSION"
+
+# Create output directory
+if [[ ! -d "./Packages/$VERSION" ]]; then
+    echo "Create directory 'Packages/$VERSION'"
+    mkdir ./Packages/$VERSION
+    if [ "$?" != "0" ]; then
+        exit
+    fi
+fi
 
 # Build packages
 for i in "${!RID_LIST[@]}"; do
@@ -29,60 +32,64 @@ for i in "${!RID_LIST[@]}"; do
     echo "[$PUB_PLATFORM ($RID)]"
     echo " "
 
+    # clean
+    rm -r ./$APP_NAME/bin/$CONFIG/net6.0/$RID
+    dotnet clean $APP_NAME
     dotnet restore $APP_NAME
     if [ "$?" != "0" ]; then
         exit
     fi
-
+    
     # build
-    dotnet msbuild $APP_NAME -t:BundleApp -property:Configuration=$CONFIG -p:SelfContained=true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:RuntimeIdentifier=$RID
+    dotnet msbuild $APP_NAME -t:BundleApp -property:Configuration=$CONFIG -p:SelfContained=true -p:PublishSingleFile=false -p:PublishTrimmed=true -p:RuntimeIdentifier=$RID
     if [ "$?" != "0" ]; then
         exit
     fi
 
     # create output directory
-    mkdir ./Packages/$PUB_PLATFORM
+    if [[ -d "./Packages/$VERSION/$PUB_PLATFORM" ]]; then
+        rm -r ./Packages/$VERSION/$PUB_PLATFORM
+    fi
+    echo "Create directory 'Packages/$VERSION/$PUB_PLATFORM'"
+    mkdir ./Packages/$VERSION/$PUB_PLATFORM
     if [ "$?" != "0" ]; then
         exit
     fi
 
     # copy .app directory to output directoty
-    mv ./$APP_NAME/bin/$CONFIG/net6.0/$RID/publish/$APP_NAME.app ./Packages/$PUB_PLATFORM/$APP_NAME.app
+    mv ./$APP_NAME/bin/$CONFIG/net6.0/$RID/publish/$APP_NAME.app ./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app
     if [ "$?" != "0" ]; then
         exit
     fi
 
     # copy application icon and remove unnecessary files
-    cp ./$APP_NAME/$APP_NAME.icns ./Packages/$PUB_PLATFORM/$APP_NAME.app/Contents/Resources/$APP_NAME.icns
+    cp ./$APP_NAME/$APP_NAME.icns ./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app/Contents/Resources/$APP_NAME.icns
     if [ "$?" != "0" ]; then
         exit
     fi
-    rm ./Packages/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/libMono*.dylib
-    rm ./Packages/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/*.png
-    rm ./Packages/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/*.pdb
+    rm ./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/libMono*.dylib
+    rm ./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/*.png
+    rm ./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/*.pdb
 
     # sign application
-    find "./Packages/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/" | while read FILE_NAME; do
+    find "./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/" | while read FILE_NAME; do
         if [[ -f $FILE_NAME ]]; then
-            codesign -f -o runtime --timestamp --entitlements "./$APP_NAME/$APP_NAME.entitlements" -s "$CERT_NAME" "$FILE_NAME"
-            if [ "$?" != "0" ]; then
-                exit
+            if [[ "$FILE_NAME" != "./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS//$APP_NAME" ]]; then
+                echo "Signing $FILE_NAME"
+                codesign -f -o runtime --timestamp --entitlements "./$APP_NAME/$APP_NAME.entitlements" -s "$CERT_NAME" "$FILE_NAME"
+                if [ "$?" != "0" ]; then
+                    exit
+                fi
             fi
         fi
     done
-    codesign -f -o runtime --timestamp --entitlements "./$APP_NAME/$APP_NAME.entitlements" -s "$CERT_NAME" ./Packages/$PUB_PLATFORM/$APP_NAME.app
-    if [ "$?" != "0" ]; then
-        exit
-    fi
+    codesign -f -o runtime --timestamp --entitlements "./$APP_NAME/$APP_NAME.entitlements" -s "$CERT_NAME" "./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app/Contents/MacOS/$APP_NAME"
+    codesign -f -o runtime --timestamp --entitlements "./$APP_NAME/$APP_NAME.entitlements" -s "$CERT_NAME" "./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app"
 
     # zip .app directory
-    cd ./Packages/$PUB_PLATFORM
-    zip -r ./$APP_NAME-$VERSION-$PUB_PLATFORM.zip ./
-    if [ "$?" != "0" ]; then
-        cd -
-        exit
-    fi
-    cd -
+    ditto -c -k --sequesterRsrc --keepParent "./Packages/$VERSION/$PUB_PLATFORM/$APP_NAME.app" "./Packages/$VERSION/$APP_NAME-$VERSION-$PUB_PLATFORM.zip"
 
 done
 
+# Generate package manifest
+dotnet run --project PackagingTool create-package-manifest osx $APP_NAME $VERSION
