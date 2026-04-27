79bba6e74fa8ebe5af2646fccc0001002471bc3f
Hamster <hamster620@gmail.com>
2026-04-27 21:09:37 +0800

    Add app info banner images.

--- PixelViewer/ViewModels/AppInfo.cs
+++ PixelViewer/ViewModels/AppInfo.cs
@@ -1,37 +1,48 @@
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using CarinaStudio;
+using CarinaStudio.AppSuite;
 using CarinaStudio.AppSuite.ViewModels;
 using System;
 using System.Collections.Generic;
 
-namespace Carina.PixelViewer.ViewModels
+namespace Carina.PixelViewer.ViewModels;
+
+/// <summary>
+/// Application info view-model.
+/// </summary>
+class AppInfo : ApplicationInfo
 {
-    /// <summary>
-    /// Application info view-model.
-    /// </summary>
-    class AppInfo : ApplicationInfo
+    // Constructor.
+    public AppInfo()
     {
-        // Constructor.
-        public AppInfo()
-        {
-            var isProVersion = this.Application.ProductManager.Let(it =>
-                !it.IsMock && it.IsProductActivated(PixelViewer.Products.Professional));
-            if (isProVersion)
-                this.Badges = new[] { this.Application.FindResourceOrDefault<IImage?>("Image/Icon.Professional").AsNonNull() };
-            else
-                this.Badges = Array.Empty<IImage>();
-        }
-        
-        
-        // Badges.
-        public override IList<IImage> Badges { get; }
-        
-        
-        // URI of project site.
-        public override Uri? GitHubProjectUri => new("https://github.com/carina-studio/PixelViewer");
-
-
-        /// <inheritdoc/>
-        public override Uri? WebsiteUri => new("https://carinastudio.azurewebsites.net/PixelViewer/");
+        var isProVersion = this.Application.ProductManager.Let(it =>
+            !it.IsMock && it.IsProductActivated(PixelViewer.Products.Professional));
+        if (isProVersion)
+            this.Badges = [ this.Application.FindResourceOrDefault<IImage?>("Image/Icon.Professional").AsNonNull() ];
+        else
+            this.Badges = Array.Empty<IImage>();
+        var baseUri = $"avares://{this.Application.Assembly.GetName().Name}";
+        using var bannerImageStream = this.Application.EffectiveThemeMode == ThemeMode.Dark
+            ? AssetLoader.Open(new Uri($"{baseUri}/AppInfoBanner-Dark.png"))
+            : AssetLoader.Open(new Uri($"{baseUri}/AppInfoBanner-Light.png"));
+        this.BannerImage = new Bitmap(bannerImageStream);
     }
+
+
+    /// <inheritdoc/>
+    public override IList<IImage> Badges { get; }
+
+
+    /// <inheritdoc/>
+    public override IImage? BannerImage { get; }
+
+
+    /// <inheritdoc/>
+    public override Uri? GitHubProjectUri => new("https://github.com/carina-studio/PixelViewer");
+
+
+    /// <inheritdoc/>
+    public override Uri WebsiteUri => new("https://carinastudio.azurewebsites.net/PixelViewer/");
 }
